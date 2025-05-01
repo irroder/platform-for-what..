8293a9fb39c2703ca15429a7f894059468fccc22
irroder <mik.ushakoff2017@yandex.ru>
2025-05-01 13:15:01 +0300

    asjhgcvk

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import { createRoot } from "react-dom/client";
-import App from "./app/App";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "app/providers/ThemeProvider";
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
+import App from './app/App';
 
-const root = createRoot(document.getElementById("root")!);
+import './shared/config/i18n/i18n';
+import { ErrorBoundary } from './app/providers/ErrorBoundary';
 
-root.render(
-	<BrowserRouter>
-		<ThemeProvider>
-			<App />
-		</ThemeProvider>
-	</BrowserRouter>
+render(
+    <BrowserRouter>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
+    </BrowserRouter>,
+    document.getElementById('root'),
 );
